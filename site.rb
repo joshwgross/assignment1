038ba9abb53a442ec96bf23d638751191fd8fb7f
Josh <freejoshy@yahoo.com>
2014-06-02 18:54:13 +0000

    Roll back to pre rest-client

--- site.rb
+++ site.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'rest-client'
 
 set :port, 4000
 set :bind, '0.0.0.0'
@@ -7,11 +6,6 @@ set :bind, '0.0.0.0'
 get '/' do
   erb :index
 end
-
- post '/' do
-     RestClient.post 'https://AC01983fd2966d4f4b28a2624b8b8abfa7:a4fd823e75768b238b222b5c19f545fa@api.twilio.com/2010-04-01/Accounts/AC01983fd2966d4f4b28a2624b8b8abfa7/SMS/Messages/', :From => '+18017846920', :To => "#{params["sms"]}", :Body => 'Hello, from u1234567'
-   erb :thanks
- end
 
   get '/italian' do
     "Ciao!"
